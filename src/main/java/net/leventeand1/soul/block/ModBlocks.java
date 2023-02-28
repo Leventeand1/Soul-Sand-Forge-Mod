155e1dbba299d7c60396ead2f81db6443b0d7ea2
Leventeand1 <stremenkatalin@gmail.com>
2023-02-28 15:58:29 +0100

    Just changed files. Nothing else.

--- src/main/java/net/leventeand1/soul/block/ModBlocks.java
+++ src/main/java/net/leventeand1/soul/block/ModBlocks.java
@@ -5,21 +5,18 @@ import net.leventeand1.soul.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.leventeand1.soul.block.RefSoulSand;
 
 import java.util.function.Supplier;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, SoulMod.MOD_ID);
 
-    public static final RegistryObject<Block> REF_SOUL_SAND = registerBlock("ref_soul_sand", () -> new Block(BlockBehaviour.Properties.of(Material.SAND)
-            .strength(2f).requiresCorrectToolForDrops().sound(SoundType.SOUL_SAND)));
+    public static final RegistryObject<Block> REF_SOUL_SAND = registerBlock("ref_soul_sand", RefSoulSand::new);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
